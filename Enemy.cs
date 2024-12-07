d47b2730288c6960502f40edb23e92b918f2c0d2
KSalmaze <pedrohsalmaze@gmail.com>
2024-12-07 20:24:27 -0300

    Classe inimigo comentada

--- Enemy.cs
+++ Enemy.cs
@@ -1,19 +1,21 @@
 namespace RunningThreads;
 
+// Classe para os inimigos 
 public class Enemy
 {
-    private char _sprite;
-    public int Lane;
-    private int _delayMilliseconds;
-    public int Position;
-    public int Health;
-    public int Damage;
-    public int Gold;
+    private char _sprite; // Sprite a ser mostrada na irteface
+    public int Lane; // Indica qual dos 3 caminhos o inimigos esta
+    private int _delayMilliseconds; // Delay entre as acoes do inimigo
+    public int Position; // Posicao horizontal do inimigo
+    public int Health; // Vida atual
+    public int Damage; // Dano
+    public int Gold; // Quantidade de ouro dada ao jogador quando o inimigo for derrotado
     
-    private InterfaceManager _interfaceManager;
-    private GameManager _gameManager;
-    private EnemySpawner _enemySpawner;
+    private InterfaceManager _interfaceManager; // Referencia a interface
+    private GameManager _gameManager; // Referencia so game manager
+    private EnemySpawner _enemySpawner; // Referencia ao spawner de inimigos
     
+    // Contrutor
     public Enemy(char sprite, int lane, int health = 3, int damage = 1,int gold = 2,int delayMilliseconds = 500)
     {
         _sprite = sprite;
@@ -26,21 +28,25 @@ public class Enemy
         Gold = gold;
         Health = health;
         Damage = damage;
-
+        
+        // Inicia paralelamente a task que controla o comportamento do inimigo
         _ = Task.Run(async () => await EnemyController());
     }
 
+    // Atualiza a representacao do inimigo na interface
     private async Task UpdatePositionOnInterface()
     {
         await _interfaceManager.AdicionarAtualizacao(_sprite, Position, Lane);
     }
 
+    // Move o imigo de posicao
     private async Task Mover()
     {
         Position -= 2;
         await UpdatePositionOnInterface();
     }
     
+    // Controla o comportamento do inimigo
     private async Task EnemyController()
     { 
         while (Health > 0)
@@ -64,11 +70,14 @@ public class Enemy
         await _interfaceManager.AdicionarAtualizacao('-', Position, Lane);
         // Dar o gold ao jogador
         await _gameManager.ChangeGold(Gold);
+        // remover da lista de inimigos
         _enemySpawner.Lanes[Lane - 1].Dequeue();
     }
 
+    // Verificar se o inimigo pode se mover
     private bool CanMove()
     {
+        // Verificar se ha algum inimigo na posicao na proxima posicao
         if (_interfaceManager.Interface[Lane][Position - 2 ] != '-')
         {
             return false;
